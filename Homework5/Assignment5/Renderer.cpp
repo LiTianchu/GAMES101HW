002e5c8126f28fa191f023adb2a3a8d177061e9c
LiTianchu <52227175+LiTianchu@users.noreply.github.com>
2024-04-25 23:49:54 +0800

    homework5 wip

--- Homework5/Assignment5/Renderer.cpp
+++ Homework5/Assignment5/Renderer.cpp
@@ -228,7 +228,26 @@ void Renderer::Render(const Scene& scene)
             // TODO: Find the x and y positions of the current pixel to get the direction
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
-            // x (horizontal) variable with the *imageAspectRatio*            
+            // x (horizontal) variable with the *imageAspectRatio*
+            //correct to center
+            x = i + 0.5f;
+            y = j + 0.5f;
+
+            //map to NDC space
+            x = x/scene.width;
+            y = y/scene.height;
+
+            x = 2 * x - 1;
+            y = 2 * y - 1;
+
+            //each pixel is stretched after the normalize as the number of screen width and height is different
+            //need to correct to square shape
+            x = x * imageAspectRatio;
+
+            //i, j is the 2D pixel coordinate, but the near plane has a certain distance to the camera
+            //so need to map this coordinate to the near plane by multiplying with the tan(fov/2)
+            x = x * scale;
+            y = y * scale;
 
             Vector3f dir = Vector3f(x, y, -1); // Don't forget to normalize this direction!
             framebuffer[m++] = castRay(eye_pos, dir, scene, 0);
